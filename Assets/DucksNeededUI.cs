eb2bb4240d4ef42937a908ee0cbe06475f386774
samsquanchone <105938519+samsquanchone@users.noreply.github.com>
2023-11-20 11:01:16 +0000

    Finished implementation of ducks needed UI
    Treated each bars worth of duck like an object containing 4 bars in a container with layout group to handle scaling.
    
    These spawn on start and set the groups image children alphas to 0 of the ducks not needed, these groups incrementally have their groups set to 1 alpha when they appear and flash to better notify the player!

--- Assets/DucksNeededUI.cs
+++ Assets/DucksNeededUI.cs
@@ -6,27 +6,73 @@ using UnityEngine.UI;
 
 public class DucksNeededUI : MonoBehaviour
 {
-    
+
     //private TMP_Text duckNeededText;
     [SerializeField] GameObject barsContainer;
 
     [SerializeField] GameObject barGroupUIPrefab; //Roughly about one ducks worth of bars 
-    List<Image> barSprites = new();
+    List<GameObject> barsObjectList = new();
+
+    Color alphaColour;
+
+    int duckStartNumber = 6; //Could change depending on game mode ect
+    int duckNeeded = 6; //Can remove this once we get the actual UI in!
 
     private void Start()
     {
+
+        alphaColour = barGroupUIPrefab.GetComponentInChildren<Image>().color;
+        alphaColour.a = 0;
         for (int i = 0; i < 10; i++)
         {
             GameObject _obj = Instantiate(barGroupUIPrefab, barsContainer.transform);
-            //Add images to list here
+            barsObjectList.Add(_obj);
+            if (i > 5)
+            {
+                Image[] bars = _obj.GetComponentsInChildren<Image>();
+                foreach (var bar in bars)
+                {
+                    bar.color = alphaColour;
+                }
+            }
         }
-    }   
 
-    int duckNeeded = 6; //Can remove this once we get the actual UI in!
+        alphaColour.a = 1; //Reset alpha to 1 here as we will only be 'adding bars' not removing them, so this avoids having to re-assign
+    }
+
+
     public void IncrementDucksNeeded()
     {
         duckNeeded += 1;
-        //duckNeededText.SetText("Ducks neeed: " + duckNeeded);
-        //Change transparencys ect ect!
+
+        //Potential issue with 9th bird, may be one out with duck needed, need to test to double check!
+        Image[] bars = barsObjectList[duckNeeded - 1].GetComponentsInChildren<Image>();
+        foreach (var bar in bars)
+        {
+            bar.color = alphaColour;
+        }
+
+        StartCoroutine("FlashBarGroup"); //Flash new bar group that appears
+    }
+
+    IEnumerator FlashBarGroup()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+        alphaColour.a = 0;
+        Image[] bars = barsObjectList[duckNeeded - 1].GetComponentsInChildren<Image>();
+        foreach (var bar in bars)
+        {
+            bar.color = alphaColour;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        alphaColour.a = 1;
+
+        foreach (var bar in bars)
+        {
+            bar.color = alphaColour;
+        }
+
     }
 }
